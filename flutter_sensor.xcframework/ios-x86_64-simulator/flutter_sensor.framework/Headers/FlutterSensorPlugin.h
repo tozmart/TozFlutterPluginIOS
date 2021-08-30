b59eeec984ed5090a5df3a5578d2ecffa9e81e8b
wangyisong <yisong.wang@tozmart.com>
2021-08-30 11:35:07 +0800

    update framework

---
+++ flutter_sensor.xcframework/ios-x86_64-simulator/flutter_sensor.framework/Headers/FlutterSensorPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterSensorPlugin : NSObject<FlutterPlugin>
+@end
---
+++ sensor.xcframework/ios-x86_64-simulator/sensor.framework/Headers/SensorPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SensorPlugin : NSObject<FlutterPlugin>
+@end
